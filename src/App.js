3ee0d4d7ed350ddf39ec22aed27f4999ac3eda84
Damian Kądziela <dkkquality@gmail.com>
2022-09-08 15:37:29 +0200

    layout components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+import ProductCard from "./components/ProductCard";
+import { Container, Grid } from "@mui/material";
+
 const App = () => (
-  <div>App</div>
-)
+    <div>
+        <Container>
+            <Grid container spacing={4}>
+                <Grid item xs={8}>
+                    <ProductCard />
+                </Grid>
+                <Grid item xs={4}>
+                    <ProductCard />
+                </Grid>
+                <Grid item xs={4}>
+                    <ProductCard />
+                </Grid>
+                <Grid item xs={4}>
+                    <ProductCard />
+                </Grid>
+                <Grid item xs={4}>
+                    <ProductCard />
+                </Grid>
+            </Grid>
+        </Container>
+    </div>
+);
 export default App;
---
+++ src/components/ProductCard.js
@@ -0,0 +1,11 @@
+import { Paper } from '@mui/material'
+
+const ProductCard = () => {
+    return (
+        <Paper elevation={8}>
+            Product Card
+        </Paper>
+    )
+}
+
+export default ProductCard;
